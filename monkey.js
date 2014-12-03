1f25b2fbe74861f199c0be3311e10d3a2b1c2f43
caitlinmonster <csteelewilliams@gmail.com>
2014-12-02 21:22:49 -0800

    added monkey to data

--- monkey.js
+++ monkey.js
@@ -1,8 +1,13 @@
+
+
 function create_card_listener($card, i) {
 		$('#game').append($card);
 		$card.find('input').on('click', function(){
 			var primateNumber = (primates[i]);
-			var buttonValue = ($(this).data());
+			var buttonValue = ($(this).data("monkey"));
+		
+
+			//var monkeyValue = buttonValue.get("monkey");
 			var monkeyStatus = (primates[i].monkey);
 			
 			//logs the value of the button
@@ -10,21 +15,49 @@ function create_card_listener($card, i) {
 			//logs the value of the monkey as true or false
 			console.log(monkeyStatus);
 			
+			var $popUpMonkey = $(
+				'<div class="correct">'+
+				'<p>That is a Monkey!</p>'+
+				'</div>'
+				)
+			var $popUpProsimian = $(
+				'<div class="wrong">'+
+				'<p>That is a Prosimian!</p>'+
+				'</div>'
+				)
+			var $popUpApe = $(
+				'<div class="wrong">'+
+				'<p>That is an Ape!</p>'+
+				'</div>'
+				)
 
-			if (buttonValue === monkeyStatus)
+			var $popUpCorrect = $(
+				'<div class="correct">'+
+				'<p>Correct!</p>'+
+				'</div>'
+				)
+
+			if (buttonValue === "Monkey")
 			{
-				alert('That is a Monkey!');
+				console.log('That is a Monkey!');
+				$($card).append($popUpCorrect);
 			} 
-			else (buttonValue !== monkeyStatus)
+			else if (buttonValue === "Prosimian")
 			{
-				alert('That is NOT a Monkey!');
+				console.log('That is NOT a Monkey! Have you never seen a Prosimian before?');
+				$($card).append($popUpProsimian);
+			} 
+			else if (buttonValue === "Ape")
+			{
+				console.log('That is NOT a Monkey! Have you never seen an Ape before?');
+				$($card).append($popUpApe);
 			}
-			// console.log()
+
 		})
 
 	}
 
-
+	
 
 	$(document).ready(function() {
 		
@@ -81,15 +114,16 @@ function create_card_listener($card, i) {
 						"<div class='opacity'></div>"+
 						"</div>"+ 
 						"<input type='button' class='button monkey' value='Monkey' data-monkey="+
-						(primates[i].type == "Monkey")+
+						(primates[i].type)+
 						">"+
 						"<input type='button' class='button not_a_monkey' value='Not a Monkey' data-monkey="+
-						(primates[i].type !== "Monkey")+
+						(primates[i].type)+
 						">"+
 						"</div>"+
 						"</div>"+
 						"</div>"+
 						"</div>")
+
 
 			    //add card to page
 			    create_card_listener($card, i)
